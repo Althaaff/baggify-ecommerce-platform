62c729e9bf147652c063491988ef3cfb4769c347
Althaaff <muhammadalthaf228@gmail.com>
2026-08-21 09:16:08 +0530

    chore: configure database and external services

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("MONGODB_URI is not set in .env");
+  }
+
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected successfully !");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+export { connectDB };
